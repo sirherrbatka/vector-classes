8c610cf11b131fd6d93e94856a2b568f76d2fc3c
Marek Kochanowlcz <mkochanowicz@ownedoutcomes.com>
2018-10-31 15:16:48 +0100

    Added eval-always for various elements extending mop.

--- src/mop.lisp
+++ src/mop.lisp
@@ -1,53 +1,54 @@
 (in-package :vector-classes)
 
+(eval-always
+  (defclass data-slot-definition (closer-mop:slot-definition)
+    ((%count-arg :initarg :count-arg
+                 :reader read-count-arg
+                 :initform nil)
+     (%scalar :initarg :scalar
+              :reader read-scalar
+              :initform t)
+     (%count-form :initarg :count-form
+                  :reader read-count-form
+                  :initform 1))))
 
-(defclass data-slot-definition (closer-mop:slot-definition)
-  ((%count-arg :initarg :count-arg
-               :reader read-count-arg
-               :initform nil)
-   (%scalar :initarg :scalar
-            :reader read-scalar
-            :initform t)
-   (%count-form :initarg :count-form
-                :reader read-count-form
-                :initform 1)))
+(eval-always
+  (defclass direct-data-slot-definition (data-slot-definition
+                                         c2mop:direct-slot-definition)
+    ()))
 
-(defclass direct-data-slot-definition (data-slot-definition
-                                       c2mop:direct-slot-definition)
-  ())
+(eval-always
+  (defclass effective-data-slot-definition (data-slot-definition
+                                            c2mop:effective-slot-definition)
+    ()))
 
+(eval-always
+  (defclass data-class (closer-mop:standard-class)
+    ()))
 
-(defclass effective-data-slot-definition (data-slot-definition
-                                          c2mop:effective-slot-definition)
-  ())
+(eval-always
+  (defmethod c2mop:validate-superclass ((class data-class)
+                                        (super c2mop:standard-class))
+    t))
 
+(eval-always
+  (defmethod c2mop:direct-slot-definition-class ((class data-class)
+                                                 &rest initargs)
+    (declare (ignore initargs))
+    (find-class 'direct-data-slot-definition)))
 
-(defclass data-class (closer-mop:standard-class)
-  ())
+(eval-always
+  (defmethod c2mop:effective-slot-definition-class ((class data-class)
+                                                    &rest initargs)
+    (declare (ignore initargs))
+    (find-class 'effective-data-slot-definition)))
 
-
-(defmethod c2mop:validate-superclass ((class data-class)
-                                      (super c2mop:standard-class))
-  t)
-
-
-(defmethod c2mop:direct-slot-definition-class ((class data-class)
-                                               &rest initargs)
-  (declare (ignore initargs))
-  (find-class 'direct-data-slot-definition))
-
-
-(defmethod c2mop:effective-slot-definition-class ((class data-class)
-                                                  &rest initargs)
-  (declare (ignore initargs))
-  (find-class 'effective-data-slot-definition))
-
-
-(defclass fundamental-data ()
-  ((%size :initarg :size
-          :reader read-size
-          :scalar t
-          :initform 0))
-  (:metaclass data-class))
+(eval-always
+  (defclass fundamental-data ()
+    ((%size :initarg :size
+            :reader read-size
+            :scalar t
+            :initform 0))
+    (:metaclass data-class)))
 
 
