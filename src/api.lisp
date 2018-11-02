9d52e5f685153a893fbda21c0908888a3c99e559
Marek Kochanowlcz <mkochanowicz@ownedoutcomes.com>
2018-11-02 12:53:24 +0100

    Added with-vector-class basic implementation.

--- src/api.lisp
+++ src/api.lisp
@@ -19,8 +19,45 @@
     instance))
 
 
-(defmacro with-vector-class ((bindings index-form &optional class) &body body)
-  `(let ,(generate-let-binding-forms bindings index-form class)
-     (declare ,@(generate-type-forms bindings index-form class))
-     (macrolet ,(generate-macrolet-binding-forms bindings index-form class)
+(eval-always
+  (defun generate-let-binding-forms (bindings instance)
+    (mapcar (lambda (form)
+              (bind (((binding slot-name) form))
+                `(,binding (slot-value ,instance ',slot-name))))
+            bindings)))
+
+
+(eval-always
+  (defun generate-type-forms (bindings class)
+    (unless (null class)
+      (let ((class (find-class class)))
+        (mapcar (lambda (form)
+                  (bind (((binding slot-name) form)
+                         (slot (slot-of-name class slot-name))
+                         (type (c2mop:slot-definition-type slot))
+                         (array (read-array slot))
+                         (fixed-dimensions (fixed-dimensions-p slot))
+                         (dimensions (read-dimensions-form slot)))
+                    (unless array
+                      (error "Slot ~a is not array in the class." slot-name))
+                    (if fixed-dimensions
+                        `(type (simple-array ,type (* ,@dimensions)) ,binding)
+                        `(type (simple-array ,type) ,binding))))
+                bindings)))))
+
+
+(eval-always
+  (defun generate-macrolet-binding-forms (bindings index-form)
+    (mapcar (lambda (form)
+              (let ((binding (car form)))
+                `(,binding
+                  (&rest args)
+                  `(aref ,,binding ,',index-form ,@args))))
+            bindings)))
+
+
+(defmacro with-vector-class ((bindings instance index-form &optional class) &body body)
+  `(let ,(generate-let-binding-forms bindings instance)
+     (declare ,@(generate-type-forms bindings class))
+     (macrolet ,(generate-macrolet-binding-forms bindings index-form)
        ,@body)))
